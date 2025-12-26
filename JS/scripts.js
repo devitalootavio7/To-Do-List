a6feee9fd0c6cb6e1315652b39fd9d879a513911
devitalootavio <italootavio266@gmail.com>
2025-12-26 14:47:35 -0300

    Update scripts.js
    apliquei JS para salvar anotações

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,9 +6,38 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-// Funções //
+// Funções 
+const saveToDo = (text) => {
 
-// Eventos //
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add('finish-todo')
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add('edit-todo')
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add('delete-todo')
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    todoInput.value = "";
+    todoInput.focus();
+};
+
+// Eventos 
 todoForm.addEventListener("submit", (e) => {
 
     e.preventDefault();
@@ -17,7 +46,6 @@ todoForm.addEventListener("submit", (e) => {
 
     if(inputValue) {
         console.log(inputValue);
-        //save ToDo
-
+        saveToDo(inputValue)
     }
 });
